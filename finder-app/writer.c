c6b11529787c7d8e8f46d8897606937036b45107
blkbny <drperreault1990@gmail.com>
2024-02-28 16:21:06 -0500

    finished writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -5,18 +5,36 @@
 int main(int argc, char *argv[])
 {
     int retval = 0;
-    
+    FILE * fp;
+
+    openlog(NULL, 0, LOG_USER);
+
     if ( argc != 3 )
     {
         // Log and return error
-        printf("Invalid number of params, expected: 3, received: %d", argc);
+        syslog(LOG_ERR, "Invalid number of arguments: %d", argc);
         retval = 1;
     }
     else
     {
+        // Log what we are writing where
+        syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
+
         // open/create file and write the second arg to it
-        retval = 0;
+        fp = fopen(argv[1], "w");
+        if ( fp == NULL )
+        {
+            syslog(LOG_ERR, "Unable to open file.");
+            retval = 1;
+        }
+        else
+        {
+            fputs(argv[2], fp);
+            fclose(fp);
+            retval = 0;
+        }
     }
 
+    closelog();
     return retval;
 }
